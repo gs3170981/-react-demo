87d824c26411736b7e562a9573af6530e50b7227
gs3170981 <1535233734@qq.com>
2018-09-10 12:09:32 +0800

    Save before ejecting

--- src/store/modules/user.js
+++
@@ -1,8 +0,0 @@
-const type = 'user'
-const data = {
-    name: ''
-}
-
-export default function (state = data, action) {
-    return action.type !== type ? state : Object.assign({}, state, action.param)
-}
